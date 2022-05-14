105418dd63fffc856eed2d42a69884d4e37d3fd3
Yacine Hadari <yhadari@e1r4p4.1337.ma>
2022-05-14 15:59:21 +0100

    modify

--- main.cpp
+++ main.cpp
@@ -4,58 +4,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// #define PORT 8080
-#define PORT 80
+class Server{
+
+  private:
+  int _server_fd;
+  struct sockaddr_in _address;
+  int _address_len;
+
+  public:
+  Server(uint16_t port, uint32_t addr) : _address_len(sizeof(_address)){
+    if ((_server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+      std::cerr << "cannot creat socket" << std::endl; 
+      exit(EXIT_FAILURE);
+    }
+
+    _address.sin_family = AF_INET;
+    _address.sin_addr.s_addr = addr;
+    _address.sin_port = htons(port);
+
+    if (bind(_server_fd, (struct sockaddr*)&_address, _address_len) < 0){
+      std::cerr << "bind does not working" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    if (listen(_server_fd, 0) < 0){
+      std::cerr << "In listen" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  void Start(){
+
+    char response[100] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    int new_socket;
+    long valread;
+
+    while(1){
+
+      std::cout << "waiting for a new connection" << std::endl;
+      if ((new_socket = accept(_server_fd, (struct sockaddr*) &_address, (socklen_t*)&_address_len)) < 0){
+        std::cerr << "In accept" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      char request[1024] = {0};
+      valread = read(new_socket, request, 1024);
+      std::cout << "--------------------------begin-------------------------" <<std::endl;
+      std::cout << request << std::endl;
+
+      write(new_socket, response, strlen(response));
+      std::cout << "Hello msg sent" << std::endl;
+      close(new_socket);
+    }
+  }
+  //int getserverfd
+};
 
 int main(int argc, char **argv){
 
   argc = 0;
   (void)argv;
 
-  int server_fd, new_socket;
-  long valread;
-  struct sockaddr_in address;
-  int address_len = sizeof(address);
-
-  char hello[20] = "Hello from server";
-  // char hello[100] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    std::cerr << "cannot creat socket" << std::endl; 
-    exit(EXIT_FAILURE);
-  }
-
-  // memset((char *)&address, 0, (size_t)address_len);
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-
-  if (bind(server_fd, (struct sockaddr*)&address, address_len) < 0){
-    std::cerr << "bind does not working" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  if (listen(server_fd, 0) < 0){
-    std::cerr << "In listen" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  while(1){
-
-    std::cout << "waiting for a new connection" << std::endl;
-    if ((new_socket = accept(server_fd, (struct sockaddr*) &address, (socklen_t*)&address_len)) < 0){
-      std::cerr << "In accept" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    char buffer[1024] = {0};
-    valread = read( new_socket , buffer, 1024);
-    std::cout << buffer << std::endl;
-
-    write(new_socket , hello , strlen(hello));
-    std::cout << "Hello msg sent" << std::endl;
-    close(new_socket);
-  }
-
+  Server server1(8080, INADDR_ANY);
+  server1.Start();
   return 0;
 }
